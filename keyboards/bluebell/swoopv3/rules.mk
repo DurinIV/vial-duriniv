95d7a8358194e21002235bc225bb5629de918465
DurinIV <n/a>
2026-05-23 17:49:47 -0400

    rebuilding keyboard from mostly scratch

---
+++ keyboards/bluebell/swoopv3/rules.mk
@@ -0,0 +1,13 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+MIDI_ENABLE = no
+SPLIT_KEYBOARD = yes
+
+LAYOUTS = split_3x5_3
