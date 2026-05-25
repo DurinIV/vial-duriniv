7e14713587e46ae33891dbbfbb7f1f34d8354b09
DurinIV <n/a>
2026-05-25 10:22:46 -0400

    swoop3 works

--- keyboards/bluebell/swoopv3/keymaps/vial-midi/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xFD, 0x91, 0xD2, 0xB9, 0xFA, 0xD5, 0x59, 0xAB}
-
-#define MIDI_ADVANCED
--- keyboards/bluebell/swoopv3/rules.mk
+++ keyboards/bluebell/swoopv3/rules.mk
@@ -4,9 +4,4 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = caterina
 
-VIA_ENABLE = yes
-
-MIDI_ENABLE = no
 SPLIT_KEYBOARD = yes
-
-LAYOUTS = split_3x5_3
