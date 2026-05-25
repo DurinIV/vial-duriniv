79976d324ddf06b3a1ba506841b66b7368d2d263
DurinIV <n/a>
2026-05-25 12:15:28 -0400

    fixing keymapping

--- keyboards/bluebell/swoopv3/keymaps/vial/keymap.c
+++ keyboards/bluebell/swoopv3/keymaps/vial/keymap.c
@@ -14,9 +14,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+extern MidiDevice midi_device;
 
 enum layers {
-    _QWERTY
+    _PUSH,
+    _PULL
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -33,10 +35,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *              `--------------------'     `--------------------'
  */
 
-[_QWERTY] = LAYOUT_split_3x5_3(
-  KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                   KC_LCTL, KC_BSPC, KC_ESC,      KC_ENT,  KC_SPC,  KC_LALT
+[_PUSH] = LAYOUT_split_3x5_3(
+  MI_E,    MI_A,    MI_Ds1,  MI_A1,   MI_As1,      MI_Ds2,  MI_A2,   MI_As2,  MI_Ds3,  MI_A3,
+  MI_C,    MI_G,    MI_C1,   MI_E1,   MI_G1,       MI_C2,   MI_E2,   MI_G2,   MI_C3,   MI_E3,
+  MI_B,    MI_D1,   MI_G1,   MI_B1,   MI_D2,       MI_G2,   MI_B2,   MI_D3,   MI_G3,   MI_B3,
+                   KC_NO,   MO(1),   KC_NO,       KC_NO,   KC_NO,   KC_NO
+),
+
+[_PULL] = LAYOUT_split_3x5_3(
+  MI_F,    MI_Bb,   MI_Eb1,  MI_G1,   MI_Bb1,      MI_Eb2,  MI_G2,   MI_Bb2,  MI_Eb3,  MI_F3,
+  MI_G,    MI_B,    MI_D1,   MI_F1,   MI_A1,       MI_B1,   MI_D2,   MI_F2,   MI_A2,   MI_B2,
+  MI_A,    MI_Gs1,  MI_A1,   MI_C2,   MI_E2,       MI_Gs2,  MI_A2,   MI_C3,   MI_E3,   MI_Gs3,
+                   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO
 )
 };
