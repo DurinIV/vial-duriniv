76ae3c8274e11fdc4db19824eb7a8abdce7d6360
DurinIV <n/a>
2026-06-03 00:20:46 -0400

    new

---
+++ keyboards/bluebell/swoopv3/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// #ifndef MATRIX_ROWS
+// #    define MATRIX_ROWS 8
+// #endif
+
+// #ifndef MATRIX_COLS
+// #    define MATRIX_COLS 5
+// #endif
+
+// #ifndef __ASSEMBLER__
+// #    include QMK_KEYBOARD_H
+// #endif
+
+// #define DIODE_DIRECTION COL2ROW
+// #define SOFT_SERIAL_PIN D2
--- keyboards/bluebell/swoopv3/keymaps/vial/keymap.c
+++ keyboards/bluebell/swoopv3/keymaps/vial/keymap.c
@@ -17,8 +17,15 @@
 extern MidiDevice midi_device;
 
 enum layers {
+<<<<<<< HEAD
+    _PULL1,
+    _PUSH1,
+    _PULL2,
+    _PUSH2
+=======
     _PUSH,
     _PULL
+>>>>>>> 79976d324ddf06b3a1ba506841b66b7368d2d263
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -35,6 +42,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *              `--------------------'     `--------------------'
  */
 
+<<<<<<< HEAD
+[_PULL1] = LAYOUT_split_3x5_3(
+  // Left Hand (Cols 1-4)      // Right Hand (Cols 5-10)
+  MI_F,    MI_C,    MI_Ds1,  MI_G1,   MI_Bb1,     MI_Ds2,  MI_G2,   MI_Bb2,  MI_Ds3,  MI_F3,
+  MI_G,    MI_B,    MI_D1,   MI_F1,   MI_A1,      MI_B1,   MI_D2,   MI_F2,   MI_A2,   MI_B2,
+  MI_A,    MI_Fs1,  MI_A1,   MI_C2,   MI_E2,      MI_Fs2,  MI_A2,   MI_C3,   MI_E3,   MI_Fs3,
+                     KC_NO,   MO(1),   MO(1),       MO(1),   MO(1),   TO(3)
+),
+[_PUSH1] = LAYOUT_split_3x5_3(
+  // Left Hand (Cols 1-4)      // Right Hand (Cols 5-10)
+  MI_E,    MI_A,    MI_Cs1,  MI_A1,   MI_Gs1,     MI_Cs2,  MI_A2,   MI_Gs2,  MI_Cs3,  MI_A3,
+  MI_C,    MI_G,    MI_C1,   MI_E1,   MI_G1,      MI_C2,   MI_E2,   MI_G2,   MI_C3,   MI_E3,
+  MI_B,    MI_D1,   MI_G1,   MI_B1,   MI_D2,      MI_G2,   MI_B2,   MI_D3,   MI_G3,   MI_B3,
+                  KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
+),
+[_PULL2] = LAYOUT_split_3x5_3(
+  MI_F,    MI_Bb,   MI_Eb1,  MI_G1,   MI_Bb1,      MI_Eb2,  MI_G2,   MI_Bb2,  MI_Eb3,  MI_F3,
+  MI_G,    MI_B,    MI_D1,   MI_F1,   MI_A1,       MI_B1,   MI_D2,   MI_F2,   MI_A2,   MI_B2,
+  MI_A,    MI_Gs1,  MI_A1,   MI_C2,   MI_E2,       MI_Gs2,  MI_A2,   MI_C3,   MI_E3,   MI_Gs3,
+                    KC_NO,   MO(1),   MO(1),       MO(1),   MO(1),   TO(0)
+),
+[_PUSH2] = LAYOUT_split_3x5_3(
+  MI_E,    MI_A,    MI_Ds1,  MI_A1,   MI_As1,      MI_Ds2,  MI_A2,   MI_As2,  MI_Ds3,  MI_A3,
+  MI_C,    MI_G,    MI_C1,   MI_E1,   MI_G1,       MI_C2,   MI_E2,   MI_G2,   MI_C3,   MI_E3,
+  MI_B,    MI_D1,   MI_G1,   MI_B1,   MI_D2,       MI_G2,   MI_B2,   MI_D3,   MI_G3,   MI_B3,
+                   KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
+=======
 [_PUSH] = LAYOUT_split_3x5_3(
   MI_E,    MI_A,    MI_Ds1,  MI_A1,   MI_As1,      MI_Ds2,  MI_A2,   MI_As2,  MI_Ds3,  MI_A3,
   MI_C,    MI_G,    MI_C1,   MI_E1,   MI_G1,       MI_C2,   MI_E2,   MI_G2,   MI_C3,   MI_E3,
@@ -47,5 +81,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MI_G,    MI_B,    MI_D1,   MI_F1,   MI_A1,       MI_B1,   MI_D2,   MI_F2,   MI_A2,   MI_B2,
   MI_A,    MI_Gs1,  MI_A1,   MI_C2,   MI_E2,       MI_Gs2,  MI_A2,   MI_C3,   MI_E3,   MI_Gs3,
                    KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO
+>>>>>>> 79976d324ddf06b3a1ba506841b66b7368d2d263
 )
 };
